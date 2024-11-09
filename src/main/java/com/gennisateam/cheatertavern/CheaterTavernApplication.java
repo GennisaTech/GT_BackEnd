9687aeb929a1cced32be3ce23d407ea03a5e343a
Administrator <565911466@qq.com>
2024-11-09 15:55:00 +0800

    feat： 新增基础配置 feature

---
+++ src/main/java/com/gennisateam/cheatertavern/CheaterTavernApplication.java
@@ -0,0 +1,13 @@
+package com.gennisateam.cheatertavern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheaterTavernApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheaterTavernApplication.class, args);
+    }
+
+}
